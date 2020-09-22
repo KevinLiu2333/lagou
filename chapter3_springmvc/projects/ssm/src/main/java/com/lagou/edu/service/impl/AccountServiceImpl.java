8db6d7ad81427d7a9800b021eadeefd64514b54c
kevinliu <liukun44919731992@gmail.com>
2020-09-22 20:50:18 +0800

    commit

---
+++ chapter3_springmvc/projects/ssm/src/main/java/com/lagou/edu/service/impl/AccountServiceImpl.java
@@ -0,0 +1,26 @@
+package com.lagou.edu.service.impl;
+
+import com.lagou.edu.mapper.AccountMapper;
+import com.lagou.edu.pojo.Account;
+import com.lagou.edu.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author liuku
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Override
+    public List<Account> queryAccountList() throws Exception {
+        return accountMapper.queryAccountList();
+    }
+}
